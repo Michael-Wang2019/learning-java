dcbe125f05d3f5aef7dc4314671060d76a342946
michael <278874356@qq.com>
2019-05-29 10:05:45 +0800

    while

--- 6.variable/src/whilevari.java
+++ 6.variable/src/whilevari.java
@@ -1,2 +1,20 @@
 public class whilevari {
+    public static void main(String[] args) {
+        int n = 10;
+
+        int dividend = 100;
+        int divisor = 173;
+
+        int found = 0;
+
+        while (found<n){
+
+            if(dividend%divisor ==0){
+
+                found++;
+                System.out.println(dividend+"可以整除"+divisor+"商为"+(dividend/divisor));
+            }
+            dividend++;
+        }
+    }
 }
