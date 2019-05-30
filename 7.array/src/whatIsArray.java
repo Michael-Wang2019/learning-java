7218e0331428a4749697a2015a46c74abf101340
michael <278874356@qq.com>
2019-05-30 10:45:08 +0800

    array

---
+++ 7.array/src/whatIsArray.java
@@ -0,0 +1,12 @@
+public class whatIsArray {
+    public static void main(String[] args) {
+        int[] book = new int[1080];
+
+        System.out.println(book[0]);
+        System.out.println(book[1]);
+
+        System.out.println("lenth " + (book.length - 1));
+
+
+    }
+}
